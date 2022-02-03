c34ef805a6c47db99c5cf2ba16eecff49ee84e55
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2022-02-03 22:40:23 +0000

    install socet.io client

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+
 import "./App.css";
+const endPoint = "https://mst-full-stack-dev-test.herokuapp.com/";
 
 function App() {
+  const [results, setResults] = useState("loading");
+  const socket = socketIOClient(endPoint);
+  useEffect(() => {
+    socket.on("data-update", (data) => {
+      console.log(data);
+      setResults(data);
+    });
+  }, []);
+
   return (
-    <div className="App">
+    <>
       <h1>MST Golf tournoment App</h1>
-    </div>
+      <p> {JSON.stringify(results)}</p>;
+    </>
   );
 }
 
