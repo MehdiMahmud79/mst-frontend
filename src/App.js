7d9873a47e42e872f41e42c33c99b364e34f7bc6
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2022-02-04 00:20:51 +0000

    fix flags

--- src/App.js
+++ src/App.js
@@ -8,33 +8,38 @@ const endPoint = "https://mst-full-stack-dev-test.herokuapp.com/";
 function App() {
   const [results, setResults] = useState(null);
   const socket = socketIOClient(endPoint);
-  const [flag, setFlag] = useState(flags[0]);
+  const [flag, setFlag] = useState("");
+  const [title, setTitle] = useState("");
 
   // this will take effect every 10 seconds as the server send new data
   useEffect(() => {
     socket.on("data-update", (data) => {
-      console.log(data);
+      // console.log(data);
       setResults(data);
-      setFlag();
-      console.log(data.Nationality);
+      setFlag(flags[data.Nationality]);
+      setTitle(data.Nationality);
     });
   }, []);
+  console.log("resul", results);
 
   return (
     <>
       <h1 className="text-red-800 text-center text-3xl font-bold p-4">
         MST Golf tournoment App
       </h1>
-
-      <ReactCountryFlag
-        countryCode="DE"
-        svg
-        style={{
-          width: "2em",
-          height: "2em",
-        }}
-        title="USA"
-      />
+      {flag ? (
+        <ReactCountryFlag
+          countryCode={flag}
+          svg
+          style={{
+            width: "4em",
+            height: "4em",
+          }}
+          title={title}
+        />
+      ) : (
+        ""
+      )}
 
       {results ? <p> {JSON.stringify(results)}</p> : "Loading..."}
     </>
