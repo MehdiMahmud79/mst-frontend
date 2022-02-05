a0707c842bd0644c6284342bbd3dee7467e6de24
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2022-02-05 16:02:48 +0000

    add sorting functionality

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -2,10 +2,11 @@ const Loader = () => {
   let circleCommonClasses = "h-2.5 w-2.5 bg-current rounded-full";
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center m-4 p-4">
       <div className="relative w-24 h-24 animate-spin rounded-full bg-gradient-to-r from-red-400 via-blue-500 to-green-400 ">
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-20 h-20 bg-gray-200 rounded-full border-2 border-white"></div>
       </div>
+      <h1 className="mx-3">LOADING...</h1>
     </div>
   );
 };
--- src/Components/TableCard.js
+++ src/Components/TableCard.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import PlayerCard from "./PlayerCard";
+import { flags } from "../utils/flags";
+import ReactCountryFlag from "react-country-flag";
 
 const TableCard = ({ player }) => {
   const [players, setPlayers] = useState([]);
+  const [sortedPlayers, setSortedPlayers] = useState([]);
+  const [highestScore, sethighestScore] = useState({});
 
   useEffect(() => {
     if (!player) return;
@@ -10,12 +14,64 @@ const TableCard = ({ player }) => {
     // remove douplicate users
     let playerSet = new Set(players);
     setPlayers([...playerSet]);
-    console.log(players);
+    let playersToSort = [...players];
+    playersToSort.sort((a, b) => b.TotalStrokes - a.TotalStrokes);
+    setSortedPlayers([...playersToSort]);
   }, [player]);
-
+  useEffect(() => {
+    sethighestScore(sortedPlayers[0]);
+  }, [sortedPlayers]);
   return (
     <div>
       <div className="bg-white p-8 rounded-md w-full">
+        {highestScore ? (
+          <div className="bg-blue-600 p-2 text-blue-100">
+            <h1>
+              Hihest Score:{" "}
+              <span className="font-bold text-gray-100 p-2">
+                {flags[highestScore.Nationality] ? (
+                  <span>
+                    <ReactCountryFlag
+                      className="mx-2 rounded-full"
+                      countryCode={flags[highestScore.Nationality]}
+                      svg
+                      style={{
+                        width: "3em",
+                        height: "2em",
+                      }}
+                      title={highestScore.Nationality}
+                    />
+                  </span>
+                ) : (
+                  <h1 className=" m-2 ">
+                    <span className="text-sm px-2 py-1 bg-yellow-300 ">
+                      {highestScore.Nationality}
+                    </span>{" "}
+                  </h1>
+                )}
+                {highestScore.TVName} :
+                <span className="font-bold text-yellow-400 p-2">
+                  {highestScore.Score}
+                </span>
+              </span>
+              <span className="font-bold text-gray-900">|</span> Rank:{" "}
+              <span className="font-bold text-gray-100 p-2">
+                {highestScore.CalculatedRankInteger}
+              </span>
+              <span className="font-bold text-gray-900">|</span> InStrokes:{" "}
+              <span className="font-bold text-gray-100 p-2">
+                {highestScore.InStrokes}
+              </span>
+              <span className="font-bold text-gray-900">|</span> Match:{" "}
+              <span className="font-bold text-gray-100 p-2">
+                {highestScore.Match}
+              </span>
+            </h1>
+          </div>
+        ) : (
+          ""
+        )}
+
         <div>
           <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
             <div className="inline-block min-w-full shadow rounded-lg ">
